82c3effaae9955cd83640ca713fb89001d70a509
Luka Korda <luka.kordic.zg@gmail.com>
2024-06-05 17:32:24 +0100

    adad

--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -1,6 +1,6 @@
 import LoginForm from "@/components/LoginForm";
 import hero from "@/public/images/hero.webp";
-import "@/styles/loginPage.scss";
+import "@/styles/LoginPage.scss";
 import { getServerSession } from "next-auth";
 import NextImage from "next/image";
 import { authOptions } from "../api/auth/[...nextauth]/route";
--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { signIn } from "next-auth/react";
 import { useState } from "react";
-import "@/styles/loginForm.scss";
+import "@/styles/LoginForm.scss";
 
 export default function LoginForm() {
   const [email, setEmail] = useState("");
