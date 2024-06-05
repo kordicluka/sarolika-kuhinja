527d2472b56807f8caf40555fca7d67d380790e2
Luka Korda <luka.kordic.zg@gmail.com>
2024-06-05 19:58:41 +0100

    adad

--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -9,9 +9,9 @@ import { redirect } from "next/navigation";
 export default async function SignIn() {
   const session = await getServerSession(authOptions);
 
-  if (session) {
-    redirect("/dashboard");
-  }
+  // if (session) {
+  //   redirect("/dashboard");
+  // }
 
   return (
     <main className="login-page">
