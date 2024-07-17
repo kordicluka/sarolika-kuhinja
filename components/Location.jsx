e96a0adb21e7b96c8aea5a6a676c9957e02d7df0
Luka Korda <luka.kordic.zg@gmail.com>
2024-07-17 13:41:35 +0100

    adad

--- components/Location.jsx
+++ components/Location.jsx
@@ -21,6 +21,24 @@ export default function Location() {
         <p>
           Ulica: Nova Ves 17, Kaptol Centar 10 000 Zagreb <br />
         </p>
+
+        <a className="btn" href="https://goo.gl/maps/" target="_blank">
+          <span>Upute do nas</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+            />
+          </svg>
+        </a>
       </div>
       <div className="location-container">
         <MapContainer />
