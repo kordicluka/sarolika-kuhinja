6dde6078fa20220e0fc1bc2152850d6ae6b1e9df
Luka Korda <luka.kordic.zg@gmail.com>
2024-07-17 13:52:10 +0100

    adad

--- components/home/Hero.jsx
+++ components/home/Hero.jsx
@@ -48,7 +48,21 @@ export default function Hero() {
           U ovoj fazi djeca će aktivno sudjelovati u kuhanju jednostavnih jela.
         </p>
         <a href="/radionice" className="btn">
-          Sve radionice
+          <span> Sve radionice</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+            />
+          </svg>
         </a>
       </div>
     </section>
