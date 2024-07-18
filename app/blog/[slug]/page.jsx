da9da13cbbfe86ecb81082d1adf646871618dd55
Luka Korda <luka.kordic.zg@gmail.com>
2024-07-18 18:35:40 +0100

    asdads

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -7,6 +7,7 @@ import Facebook from "@/components/Facebook";
 import Instagram from "@/components/Instagram";
 import WhatsApp from "@/components/WhatsUpp";
 import ItemContent from "@/components/ItemContent";
+import CopyToClipboard from "@/components/CopyToClipboard";
 
 export default async function BlogPage({ params }) {
   const { slug } = params;
@@ -80,24 +81,7 @@ export default async function BlogPage({ params }) {
             <a href="#" className="social">
               <WhatsApp />
             </a>
-            <button className="share-button">
-              {" "}
-              <span>Podijeli</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z"
-                />
-              </svg>
-            </button>
+            <CopyToClipboard />
           </div>
         </div>
       </section>
