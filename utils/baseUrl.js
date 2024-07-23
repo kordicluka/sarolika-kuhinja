a90b945e1076741e8cefba12775ad56e5153878d
Luka Kordić <luka.kordic@msk.hr>
2024-07-23 22:52:49 +0200

    adad

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,4 +1,3 @@
 // utils/baseUrl.js
 
-export const baseUrl =
-  process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'
+export const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://test.msk.hr/'
--- utils/db.js
+++ utils/db.js
@@ -1,10 +1,10 @@
 // utils/db.js
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global;
+const globalForPrisma = global
 
-const prisma = globalForPrisma.prisma || new PrismaClient();
+const prisma = globalForPrisma.prisma || new PrismaClient()
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
-export default prisma;
+export default prisma
