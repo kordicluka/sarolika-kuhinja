772667cdff160e20543ec2b023820facebfa6745
Luka Kordić <luka.kordic@msk.hr>
2024-07-23 19:33:53 +0200

    adad

--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -1,26 +1,20 @@
-import LoginForm from "@/components/dashboard/LoginForm";
-import hero from "@/public/images/hero.webp";
-import "@/styles/LoginPage.scss";
-import { getServerSession } from "next-auth";
-import NextImage from "next/image";
-import { authOptions } from "../api/auth/[...nextauth]/route";
-import { redirect } from "next/navigation";
-import ToasterProvider from "@/components/dashboard/ToasterProvider";
+import LoginForm from '@/components/dashboard/LoginForm'
+import hero from '@/public/images/hero.webp'
+import '@/styles/LoginPage.scss'
+import { getServerSession } from 'next-auth'
+import NextImage from 'next/image'
+import { authOptions } from '../api/auth/[...nextauth]/route'
+import { redirect } from 'next/navigation'
+import ToasterProvider from '@/components/dashboard/ToasterProvider'
 
 export default async function SignIn() {
-  const session = await getServerSession(authOptions);
-
-  if (session) {
-    redirect("/dashboard");
-  }
-
   return (
     <>
       <main className="login-page">
-        {" "}
+        {' '}
         <LoginForm />
       </main>
       <ToasterProvider />
     </>
-  );
+  )
 }
